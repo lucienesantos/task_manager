9fff2b79d15ac81b4a7cdf15b35d4d04789dcdae
Luciene Santos <lucienesantosmc@gmail.com>
2017-01-06 16:39:27 -0300

    Revert "Add Makro from current list"
    This reverts commit afef12da9ad6ee625569b6f92a84e59c9adb87bb.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
 
-  delete '/keeps/remove_many' => 'keeps#destroy_many'
+  delete "/keeps/remove_many" => "keeps#destroy_many"
 
   resources :keeps, except: [:new] do
     post 'start', on: :member, to: 'keeps#start', as: :start
   end
   
   get 'makros/:id', to: 'makros#show', as: :makro
-  post 'makros' => 'makros#create'
-  get 'makros' => 'makros#index'
 
   namespace :api do
     resources :keeps, only: [:index]
--- spec/controllers/makros_controller_spec.rb
+++ spec/controllers/makros_controller_spec.rb
@@ -2,55 +2,6 @@ require 'rails_helper'
 
 RSpec.describe MakrosController, type: :controller do
 
-  let(:valid_attributes) { {name: "My new Makro"} }
-  let(:invalid_attributes) { {name: ""} }
-  
-  describe "GET #index" do
-    let!(:makro_clean_house){ FactoryGirl.create(:makro, name: 'Clean House') }
-    let!(:makro_get_things_done){ FactoryGirl.create(:makro, name: 'Get things done') }
-
-    it "returns http success" do
-      get :index, format: :json
-      expect(response).to have_http_status(:success)
-    end
-
-    it "returns all makros as json" do
-      get :index, format: :json
-      json = JSON.parse(response.body)
-      expect(json.count).to eq(2)
-    end
-  end
-
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new Makro" do
-        expect {
-          post :create, params: {makro: valid_attributes}, format: :json
-        }.to change(Makro, :count).by(1)
-      end
-
-      it "assigns a newly created makro as @makro" do
-        post :create, params: {makro: valid_attributes}, format: :json
-        expect(assigns(:makro)).to be_a(Makro)
-        expect(assigns(:makro)).to be_persisted
-      end
-
-      it "renders a a makro as json" do
-        post :create, params: {makro: valid_attributes}, format: :json
-        json = JSON.parse(response.body)
-        expect(json["name"]).to eq("My new Makro")
-      end
-    end
-
-    context "with invalid params" do
-      it "assigns a newly created but unsaved makro as @makro" do
-        post :create, params: {makro: invalid_attributes}, format: :json
-        expect(assigns(:makro)).to be_a_new(Makro)
-        expect(assigns(:makro)).not_to be_persisted
-      end
-    end
-  end
-
   describe "GET #show" do
     let(:makro){ FactoryGirl.create :makro }
     
